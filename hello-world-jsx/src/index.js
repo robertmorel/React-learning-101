cb99001a5c0eed2ab72a83bdb0cefe3c30f5e5bd
rmorel <rmorel32@yahoo.com>
2018-09-22 13:19:49 +0100

    Hello jsx code added

---
+++ hello-world-jsx/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import HelloWorld from "./components/helloWorldComp";
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+//registerServiceWorker();
