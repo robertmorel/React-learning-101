bd4d70fa1dcfb44f3de7284d584467ef888f277f
rmorel <rmorel32@yahoo.com>
2018-10-06 17:53:26 +0100

    New posts component

---
+++ apps/blog-posts/src/components/post.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { getPosts } from "../services/blogPostList";
+
+class Post extends Component {
+  state = {
+    posts: getPosts()
+  };
+
+  listOfPosts() {
+    //map tags to list or display message
+    if (this.state.posts.length === 0) return <p>There are no posts!</p>;
+    return "List of posts";
+  }
+
+  handleDeletePost = post => {
+    const posts = this.state.posts.filter(p => p._id !== post._id);
+    this.setState({ posts });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>{this.listOfPosts()}</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Category</th>
+              <th>Content</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map(post => (
+              <tr key={post._id}>
+                <td>{post.title}</td>
+                <td>{post.author}</td>
+                <td>{post.category.name}</td>
+                <td>{post.content}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDeletePost(post)}
+                    className="btn btn-danger"
+                  >
+                    Delete Post
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Post;
--- apps/blog-posts/src/components/posts.jsx
+++ apps/blog-posts/src/components/posts.jsx
@@ -1,56 +1,12 @@
 import React, { Component } from "react";
-import { getPosts } from "../services/blogPostList";
+import Post from "./post";
 
 class Posts extends Component {
-  state = {
-    posts: getPosts()
-  };
-
-  listOfPosts() {
-    //map tags to list or display message
-    if (this.state.posts.length === 0) return <p>There are no posts!</p>;
-    return "List of posts";
-  }
-
-  handleDeletePost = post => {
-    console.log(post);
-    const posts = this.state.posts.filter(m => m._id !== post._id);
-    this.setState({ posts });
-  };
-
+  state = {};
   render() {
     return (
       <div>
-        <h2>{this.listOfPosts()}</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author</th>
-              <th>Category</th>
-              <th>Content</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.posts.map(post => (
-              <tr key={post._id}>
-                <td>{post.title}</td>
-                <td>{post.author}</td>
-                <td>{post.category.name}</td>
-                <td>{post.content}</td>
-                <td>
-                  <button
-                    onClick={() => this.handleDeletePost(post)}
-                    className="btn btn-danger"
-                  >
-                    Delete Post
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <Post />
       </div>
     );
   }
